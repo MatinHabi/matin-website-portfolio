1ef8d82fad0d908863062cd3fd1202f63f5244c5
MatinHabi <a1888637@adelaide.edu.au>
2025-07-17 16:25:54 +0930

    Initial commit with all files

---
+++ script.js
@@ -0,0 +1,21 @@
+function isInView(element){
+    const rect = element.getBoundingClientRect();
+    return(
+        rect.top <= (window.innerHeight -200) && rect.bottom >= 0
+    );
+}
+
+function handleScroll(){
+    const sections = document.querySelectorAll('.fade-in-section');
+    sections.forEach((section) => {
+        if (isInView(section)) {
+          section.classList.add('visible');
+        }else{
+            section.classList.remove('visible');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
+
